beb5126a55537b74f5a9c4f24621412fb3d45002
chengang <1410488687@qq.com>
2018-09-11 19:07:55 +0800

    add some model

--- XShop/settings.py
+++ XShop/settings.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+sys.path.insert(0,BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -25,6 +28,8 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
+
+AUTH_USER_MODEL='users.UserProfile'
 
 
 # Application definition
